d58eed4b112bdf389e034e9c7d91e93d52d125b1
tz01 <36091788+tz01@users.noreply.github.com>
2019-06-01 23:09:00 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,11 @@
-function getState(){
-	var a=document.getElementById("states").value;
-	document.getElementById("demo").innerHTML = "You selected: " + a;
-}
+var url = "https://developer.nps.gov/api/v1/visitorcenters?stateCode=NJ&api_key=ggvKIJXPJksoGFESOfN9FcpDKLkPW7gTXQU47x24";
 
-function getVisitor(){
-	//send GET request to National Parks API
-	$(function(){
-		$.ajax({
-			type: 'GET',
-			url:'https://developer.nps.gov/api/v1/visitorcenters?stateCode=NJ&api_key=ggvKIJXPJksoGFESOfN9FcpDKLkPW7gTXQU47x24'
-			success: function(data){
-				console.log('success', data);
+$.ajax({
+	type: 'GET',
+	url: url;
+	success: function(data){
+		console.log('success', data);
 			}
 		});
-	});
+
 	
