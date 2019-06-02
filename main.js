430dd5674333504352150a4ce4f882b6649e635b
tz01 <36091788+tz01@users.noreply.github.com>
2019-06-02 23:19:27 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,27 @@
-$.ajax({
-	type: 'GET',
-	url: 'https://developer.nps.gov/api/v1/visitorcenters?stateCode=NJ&api_key=ggvKIJXPJksoGFESOfN9FcpDKLkPW7gTXQU47x24',
-	success: function(data){
-		console.log(data);
-			},
-	});
+//$('#msg').html(msg + icon)
+$(document).ready(function () {
+  $("#submit").click(function (e) {
+      var validate = Validate();
+      $("#demo").html(validate)
+      if (validate.length === 0) {
+        $.ajax({
+	        type: 'GET',
+        	url: 'https://developer.nps.gov/api/v1/visitorcenters?stateCode='+$("#states").val()+'&api_key=ggvKIJXPJksoGFESOfN9FcpDKLkPW7gTXQU47x24',
+	        datatype:'json',
+	        success: function(data){
+		        console.log(data);
+			    },
+      	});
+      }
+    });
+
+      	
+    function Validate() {
+        var errorMessage = "";
+        if ($("#states").val() == "Select") {
+            errorMessage += "► Select State";
+        }
+        return errorMessage;
+    }
+});
+
