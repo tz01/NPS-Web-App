efa3e5a4a4275834208853d296cb87e844303a93
tz01 <36091788+tz01@users.noreply.github.com>
2019-06-02 12:50:16 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
-var url = "https://developer.nps.gov/api/v1/visitorcenters?stateCode=NJ&api_key=ggvKIJXPJksoGFESOfN9FcpDKLkPW7gTXQU47x24";
-
 $.ajax({
 	type: 'GET',
-	url: url;
+	url: 'https://developer.nps.gov/api/v1/visitorcenters?stateCode=NJ&api_key=ggvKIJXPJksoGFESOfN9FcpDKLkPW7gTXQU47x24',
 	success: function(data){
-		console.log('success', data);
-			}
-		});
+		console.log(data);
+			},
+	});
