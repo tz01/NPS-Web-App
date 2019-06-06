fe134bfc451860fd1aed298531bc46bece702ead
tz01 <36091788+tz01@users.noreply.github.com>
2019-06-06 14:36:10 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
-//$('#msg').html(msg + icon)
+var names= {};
+var description ={};
+const app = document.getElementById('root');
+const container = document.createElement('div');
+container.setAttribute('class', 'container');
+app.appendChild(container);
+
 $(document).ready(function () {
   $("#submit").click(function (e) {
       var validate = Validate();
@@ -6,10 +12,27 @@ $(document).ready(function () {
       if (validate.length === 0) {
         $.ajax({
 	        type: 'GET',
-        	url: 'https://developer.nps.gov/api/v1/visitorcenters?stateCode='+$("#states").val()+'&api_key=ggvKIJXPJksoGFESOfN9FcpDKLkPW7gTXQU47x24',
+        	url: 'https://developer.nps.gov/api/v1/parks?stateCode='+$("#states").val()+'&api_key=ggvKIJXPJksoGFESOfN9FcpDKLkPW7gTXQU47x24',
 	        datatype:'json',
 	        success: function(data){
-		        console.log(data);
+            var info=data["data"];
+            for(var i = 0; i < 50 && i < info.length; i++){
+              names[i] = info[i]["fullName"];
+              description[i]=info[i]["description"];
+            }
+            for (var key in names) {
+              var msg= ("Park " + key + ": " + names[key]);
+              const card = document.createElement('div');
+              card.setAttribute('class', 'card');
+
+               const h1 = document.createElement('h1');
+               h1.textContent =msg;
+          
+               container.appendChild(card);
+               card.appendChild(h1);
+            }
+            console.log(description);
+            console.log("success");
 			    },
       	});
       }
@@ -24,4 +47,5 @@ $(document).ready(function () {
         return errorMessage;
     }
 });
+
 
