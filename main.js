6c1be7c5fbe71a298f9895e2e85cd290100294dc
tz01 <36091788+tz01@users.noreply.github.com>
2019-06-12 22:17:08 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
-var names= {};
-var description ={};
-const app = document.getElementById('root');
-const container = document.createElement('div');
-container.setAttribute('class', 'container');
-app.appendChild(container);
+var names= [];
+var descriptions =[];
+var parkCodes=[];
+var result={};
+var imagesUrls=[];
 
 $(document).ready(function () {
   $("#submit").click(function (e) {
@@ -12,33 +11,46 @@ $(document).ready(function () {
       if (validate.length === 0) {
         $.ajax({
 	        type: 'GET',
-        	url: 'https://developer.nps.gov/api/v1/parks?stateCode='+$("#states").val()+'&api_key=ggvKIJXPJksoGFESOfN9FcpDKLkPW7gTXQU47x24',
+        	url: 'https://developer.nps.gov/api/v1/parks?stateCode='+$("#states").val()+'&fields=images&api_key=ggvKIJXPJksoGFESOfN9FcpDKLkPW7gTXQU47x24',
 	        datatype:'json',
 	        success: function(data){
-            var info=data["data"];
-            for(var i = 0; i < 50 && i < info.length; i++){
-              names[i] = info[i]["fullName"];
-              description[i]=info[i]["description"];
-            }
-            for (var key in names) {
-              var msg= ("Park " + key + ": " + names[key]);
-              const card = document.createElement('div');
-              card.setAttribute('class', 'card');
-
-               const h1 = document.createElement('h1');
-               h1.textContent =msg;
-          
-               container.appendChild(card);
-               card.appendChild(h1);
-            }
-            console.log(description);
-            console.log("success");
+	          getInfo(data);
+	          for (var url in imagesUrls){
+	            var elem = document.createElement("img");
+              elem.src = url;
+              document.getElementById("display").appendChild(elem);
+	          }
 			    },
       	});
       }
     });
+    
+    //Returns necessary information   
+    function getInfo(data){
+      var info=data["data"];
+      for(var i = 0; i < 50 && i < info.length; i++){
+        names[i] = info[i]["fullName"];
+        descriptions[i]=info[i]["description"];
+        parkCodes[i]=info[i]["parkCode"];
+        imagesUrls[i]=info[i]["images"][0]["url"];
+      }
+        var currentKey;
+        var currentVal;
+        for (var x = 0; x < names.length; x++) {
+          currentKey = (names[x]);
+          currentVal = (parkCodes[x]);
+          result[currentKey] = currentVal; 
+      }
+      for (var key in names) {
+        var park_name= names[key];
+        console.log(park_name);
+      }
+      for (var park in descriptions){
+        var park_description=descriptions[park];
+      }
+    }
 
-      	
+    //Checks to see if user has entered a value
     function Validate() {
         var errorMessage = "";
         if ($("#states").val() == "Select") {
@@ -46,6 +58,12 @@ $(document).ready(function () {
         }
         return errorMessage;
     }
+
 });
+//Background image
+//On submit, user should automatically slide down to table with park, image, and description should appear. The table should be resizable.
+//On click of picture, user should automatically slide down. Info should be displayyed with Tabs. 
+//https://jqueryui.com/tabs/#vertical
+
 
 
